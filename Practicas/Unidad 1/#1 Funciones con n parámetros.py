d1505ee9a795d84621407e0ec38f72c1d6f4df7c
Barrabasito <juan72764@hotmail.com>
2023-09-27 19:10:43 -0500

    1 terminado

--- Practicas/Unidad 1/#1 Funciones con n parámetros.py
+++ Practicas/Unidad 1/#1 Funciones con n parámetros.py
@@ -1,7 +1,6 @@
 # Escribir un programa que contenga una función que reciba n parámetros de tipo numérico y calcule el
 # producto total y su suma total
 
-
 def CalcularProductoTotal(*args):
     TotalProduc = 1
     TotalSuma = 0
@@ -10,19 +9,22 @@ def CalcularProductoTotal(*args):
         TotalSuma += n
     return TotalSuma, TotalProduc
 
-
 Parametros = []
-Contador = 1
-while Contador == 1:
-    num = int(input("Favor de ingresar un valor: "))
-    if num <= 0:
-        print("Favor de ingresar un numero valido")
-    Parametros.append(num)
-    Contador = int(input("¿Quiere seguir agregando numeros? 1.Si 2.No "))
+
+#Validar entrada
+while True:
+    try:
+        num = float(input("Ingresa un valor de tipo numérico: "))
+        Parametros.append(num)
+        seguir = input("Si desea terminar de ingresar escriba \"exit\" sino da enter: ")
+        if seguir.lower()=='exit':
+            break
+    except ValueError:
+        print("Debes ingresar un valor de tipo numérico.")
 
 if len(Parametros) <= 1:
-    print("No se realizaron las operaciones ya que no se ingreso mas de 1 numero")
+    print("No se realizaron las operaciones ya que no se ingreso más de 1 número")
 else:
     TotalSuma, TotalProducto = CalcularProductoTotal(*Parametros)
-    print(f"El total del producto es {TotalProducto}")
-    print(f"El total del producto es {TotalSuma}")
+    print(f"El total del producto es: {TotalProducto}")
+    print(f"La suma total es: {TotalSuma}")
