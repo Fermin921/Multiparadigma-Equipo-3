8c5daa037041094ddf44108050ccf7aa1f299115
Barrabasito <juan72764@hotmail.com>
2023-09-27 19:59:29 -0500

    2 terminado

--- Practicas/Unidad 1/#2 Manejo y manipulación de elementos de una lista.py
+++ Practicas/Unidad 1/#2 Manejo y manipulación de elementos de una lista.py
@@ -1,10 +1,13 @@
 # Escribir un programa que almacene el abecedario en una lista, elimine de la lista las letras que ocupen
 # posiciones múltiplos de 2, y muestre por pantalla la lista resultante
 
-ListaResultante = []
+ListaParaEliminar = []
 Abecedario = [chr(i) for i in range(97, 123)]
 for i in range(len(Abecedario)):
-    if ((i + 1) % 2) == 0:
-        ListaResultante.append(Abecedario[i])
+    if (i % 2) == 0:
+        ListaParaEliminar.append(Abecedario[i-1])
 
-print(ListaResultante)
+for f in ListaParaEliminar:
+    Abecedario.remove(f)
+
+print(Abecedario)
