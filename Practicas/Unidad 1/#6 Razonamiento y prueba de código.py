f998cc3dbe7facf53066f25856a820269ec89598
Barrabasito <juan72764@hotmail.com>
2023-09-27 21:13:43 -0500

    6 terminado

--- Practicas/Unidad 1/#6 Razonamiento y prueba de código.py
+++ Practicas/Unidad 1/#6 Razonamiento y prueba de código.py
@@ -1,29 +1,7 @@
 # Escribir un programa que reciba un numero entre 0 y 20 e imprimir el numero en letra, no utilizar
 # condicionales, máximo 5 líneas de código
 
-numletras = [
-    "cero",
-    "uno",
-    "dos",
-    "tres",
-    "cuatro",
-    "cinco",
-    "seis",
-    "siete",
-    "ocho",
-    "nueve",
-    "diez",
-    "once",
-    "doce",
-    "trece",
-    "catorce",
-    "quince",
-    "dieciséis",
-    "diecisiete",
-    "dieciocho",
-    "diecinueve",
-    "veinte",
-]
+numletra = {0: 'cero', 1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco', 6: 'seis', 7: 'siete', 8: 'ocho', 9: 'nueve', 10: 'diez', 11: 'once', 12: 'doce', 13: 'trece', 14: 'catorce', 15: 'quince', 16: 'dieciséis', 17: 'diecisiete', 18: 'dieciocho', 19: 'diecinueve', 20: 'veinte'}
 
 numero = int(input("Ingrese un número entre 0 y 20: "))
-print(numletras[numero])
+print(numletra.get(numero, "Número no válido"))
